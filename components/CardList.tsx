e4a89be3c342e49bcca0ab045cc199fa02045671
Sektio Nugroho <128130308+sektinugros@users.noreply.github.com>
2024-10-07 13:53:51 +0700

    Delete components directory

--- components/CardList.tsx
+++
@@ -1,9 +0,0 @@
-const CardList = ({children}: Readonly<{children: React.ReactNode}>) => {
-    return (
-      <div className="w-full p-4">
-          {children}
-      </div>
-    )
-  }
-  
-  export default CardList
